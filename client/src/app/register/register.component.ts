479f04943453a2db762923e6ac8c242be17a5019
DESKTOP-V8M24KM\Jinelin Briones <jerome.briones@corptech.it>
2024-09-16 22:42:06 +0800

    End of Section 10

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject, input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountsService } from '../_services/accounts.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  private accountService = inject(AccountsService);
+  private toastr = inject(ToastrService);
+  cancelRegister = output<boolean>();
+  model: any = {};
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => this.toastr.error(error.error)
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
